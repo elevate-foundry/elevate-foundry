7037ae5c0f6de7e2439b480c4266179539098257
Ryan Barrett <95192191+elevate-foundry@users.noreply.github.com>
2024-04-30 13:26:24 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/send_message', methods=['POST'])
+@app.route('/send_message', methods=['POST'])  # Specify methods=['POST'] to allow POST requests
 def send_message():
-    data = request.get_json()
-    message = data['message']
-    # Create a JSON response containing the received message
-    response = {'response': f'Message received: {message}'}
-    return jsonify(response)
+    if request.method == 'POST':
+        data = request.get_json()
+        message = data['message']
+        # Create a JSON response containing the received message
+        response = {'response': f'Message received: {message}'}
+        return jsonify(response)
+    else:
+        # Return a 405 error response if a non-POST request is received
+        return jsonify({'error': 'Method Not Allowed'}), 405
 
 if __name__ == '__main__':
     app.run(debug=True)
